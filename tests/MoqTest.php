50c58c3955ce06f37c8890410b410be912773fb1
Dmitry Fitiskin <d.fitiskin@gmail.com>
2010-02-21 10:47:42 +0400

    Теперь все тесты работают с мок-менеджером созданном в setUp и вызывают методы мок-объекта, а не менеджера

--- tests/MoqTest.php
+++ tests/MoqTest.php
@@ -27,41 +27,35 @@ class MoqTest extends PHPUnit_Framework_TestCase
     
     public function testGetArguments()
     {
-        $moq = new Moq();
-        
-        $moq->execute('this is test argument');
+        $this->moq->object->execute('this is test argument');
         
         $this->assertEquals(
             array('this is test argument'),
-            $moq->getArguments('execute', 0)
+            $this->moq->getArguments('execute', 0)
         );
     }
     
     public function testGetArgument()
     {
-        $moq = new Moq();
-        
-        $moq->execute('this is test argument');
+        $this->moq->object->execute('this is test argument');
         
         $this->assertEquals(
             'this is test argument',
-            $moq->getArgument('execute', 0, 0)
+            $this->moq->getArgument('execute', 0, 0)
         );
     }
     
     public function testGetCallCount()
     {
-        $moq = new Moq();
-        
         $count = 5;
         for ($i = 0; $i < $count; $i++)
         {
-            $moq->execute();
+            $this->moq->object->execute();
         }
         
         $this->assertEquals(
             $count,
-            $moq->getCallCount('execute')
+            $this->moq->getCallCount('execute')
         );
     }
 }
