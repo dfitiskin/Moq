4d5463ac6c040e643f008ca3b59f85120b9397ac
Dmitry Fitiskin <d.fitiskin@gmail.com>
2010-02-21 10:50:28 +0400

    Отрефакторил тест-кейс. Теперь есть прямой доступ к двум объектам manager и object.

--- tests/MoqTest.php
+++ tests/MoqTest.php
@@ -5,43 +5,44 @@ include_once('moq.php');
 
 class MoqTest extends PHPUnit_Framework_TestCase
 {
-    private $moq = null;
+    private $manager = null;
     
     public function setUp()
     {
-        $this->moq = new Moq();
+        $this->manager = new Moq();
+        $this->object = $this->manager->object;
     }
     
     public function testAddMethodSimple()
     {
-        $this->moq->addMethod(
+        $this->manager->addMethod(
             'execute',
             3
         );
         
         $this->assertEquals(
             3,
-            $this->moq->object->execute()
+            $this->object->execute()
         );
     }
     
     public function testGetArguments()
     {
-        $this->moq->object->execute('this is test argument');
+        $this->object->execute('this is test argument');
         
         $this->assertEquals(
             array('this is test argument'),
-            $this->moq->getArguments('execute', 0)
+            $this->manager->getArguments('execute', 0)
         );
     }
     
     public function testGetArgument()
     {
-        $this->moq->object->execute('this is test argument');
+        $this->object->execute('this is test argument');
         
         $this->assertEquals(
             'this is test argument',
-            $this->moq->getArgument('execute', 0, 0)
+            $this->manager->getArgument('execute', 0, 0)
         );
     }
     
@@ -50,12 +51,12 @@ class MoqTest extends PHPUnit_Framework_TestCase
         $count = 5;
         for ($i = 0; $i < $count; $i++)
         {
-            $this->moq->object->execute();
+            $this->object->execute();
         }
         
         $this->assertEquals(
             $count,
-            $this->moq->getCallCount('execute')
+            $this->manager->getCallCount('execute')
         );
     }
 }
